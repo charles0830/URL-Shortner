70ef04032e8e83ab1516df4ab9f462e2af510d8b
charles0830 <bhgold830@gmail.com>
2021-01-01 21:13:35 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/another', (req, res) => {
+    res.send('Hello From Another World!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
