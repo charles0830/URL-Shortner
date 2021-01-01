5e52cbb196847e14f71f5a38fc782fc8a28ac67e
charles0830 <bhgold830@gmail.com>
2021-01-02 02:45:49 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ const app = express()
 const port = 3000
 
 const admin = require("firebase-admin");
-const serviceAccount = require("url-shortner-338c1-firebase-adminsdk-yfrrz-cc29eab9ce.json");
+const serviceAccount = require("./url-shortner-338c1-firebase-adminsdk-yfrrz-cc29eab9ce.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-
 const staticFolder = express.static("public");
+
+const urlsdb = admin.firestore().collection("urlsdb")
+
 app.use(staticFolder);
 app.use(bodyParser.json());
 
 app.get('/:short_url', (req,res) => {
     console.log(res.params.short_url);
     const short_url = res.params.short_url;
-    res.send(short_url);
+    const doc = urlsdb.doc(short_url)
+
+    doc.get().then(response=>{
+        const data = response.data()
+        if (data && data.url){
+            res.redirect(301,data.url);
+        }
+    })
 })
 
 app.post('/admin/urls/', (req, res) => {
