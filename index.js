691cbcbcd9f1cdc99701241d620182b0f8d87466
charles0830 <bhgold830@gmail.com>
2021-01-02 00:18:29 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const staticFolder = express.static("public");
 
-app.use(staticFolder)
+app.use(staticFolder);
+app.use(bodyParser.json());
 
 
-app.get('/another', (req, res) => {
+app.post('/admin/urls/', (req, res) => {
+    console.log(req.body);
     res.send('Hello From Another World!')
 })
 
