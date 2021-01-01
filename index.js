b468c00c2f1ec46ada220a7c0ec4d1f5cb2435df
charles0830 <bhgold830@gmail.com>
2021-01-01 22:29:28 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const staticFolder = express.static("public");
+
+app.use(staticFolder)
+
 
 app.get('/another', (req, res) => {
     res.send('Hello From Another World!')
